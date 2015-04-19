3edaf13f0a2b2009bf1026b536bda1e8272fd787
stefanbanu <stefanbanu1@gmail.com>
2015-04-19 13:08:31 +0300

    tutorial complete

--- stefan-chat/index.js
+++ stefan-chat/index.js
@@ -8,10 +8,22 @@ var io = require('socket.io')(http);
 
 http.port = 3002;
 
-io.on('connection', function(socket){
+/*io.on('connection', function(socket){
   console.log('a user connected');
      socket.on('disconnect', function(){
     console.log('user disconnected');
+  });
+});*/
+
+/*io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});*/
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
   });
 });
 
