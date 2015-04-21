54d424572a7ec063287a5e3f34148d4552eb10fa
Eric Griffith <eric.griffith42@gmail.com>
2015-04-21 16:37:34 -0500

    completed tutorial

--- eric/server.js
+++ eric/server.js
@@ -1,18 +1,46 @@
+// Initialize function handler, app, using Express
+// and apply it to out HTTP server, http
 var app = require('express')();
 var http = require('http').Server(app);
 
+// Initialize Socket.IO instance to listen on our server
+var io = require('socket.io')(http);
+
+// Define server port
 http.port = 3000;
 
+// Define route handler to call when server is accessed
 app.get('/', function(req, res){
-  res.send('<h1>Hello world from /</h1>');
-  console.log("Accessed: /");
+  res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/chat-project', function(req, res){
-  res.send('<h1>Hello world from /chat-project</h1>');
-  console.log("Accessed: /chat-project");
+// Log 'connection' and 'disconnect' socket messages to server
+/*
+io.on('connection', function(socket) {
+   console.log('a user connected');   
+   socket.on('disconnect', function() {
+      console.log('a user disconnected');
+   });
+});
+*/
+
+// Log 'chat message' socket messages to server
+/*
+io.on('connection', function(socket) {
+   socket.on('chat message', function(msg) {
+      console.log('message: ' + msg);
+   });
+});
+*/
+
+// Send 'chat message' sockets to client (browser)
+io.on('connection', function(socket) {
+   socket.on('chat message', function(msg) {
+      io.emit('chat message', msg);
+   });
 });
 
+// Tell server to listen on port defined by http.port
 http.listen(http.port, function(){
   console.log('listening on *:' + http.port);
 });
