ddcbac65d3ab8b208ae33d7deb70ba691be5ccb5
Josh <joshmead4@yahoo.com>
2015-05-31 13:42:15 -0500

    Added flood control and a cool animation
    No more spam :D

--- joshmead/index.js
+++ joshmead/index.js
@@ -8,6 +8,11 @@ var path = require('path');
 var connectedUsers = [];
 var guestIndex = 1;
 
+//Flood Control ----------------------------------------------------------------
+//Allow max of 7 messages per 10 seconds before dropping the rest of the messages
+var messageRate = 7.0;
+var messagePer = 10.0;
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
@@ -27,18 +32,38 @@ io.on('connection', function(socket){
 	});
    
     socket.on('chat message', function(msg){
-        socket.broadcast.emit('chat message', msg);
+	    //Flood Control
+	    for(var i=0; i<connectedUsers.length; i++) {
+		    if(socket.id == connectedUsers[i].ID) {		
+				var currentTime = Date.now();
+				var timeElapsed = (currentTime - connectedUsers[i].Check) / 1000.0;
+				connectedUsers[i].Check = currentTime;
+				//console.log("Before:  " + connectedUsers[i].Allowance)
+				connectedUsers[i].Allowance += timeElapsed * (messageRate / messagePer);
+				//console.log("After:   " + connectedUsers[i].Allowance)
+				if(connectedUsers[i].Allowance > messageRate)
+				    connectedUsers[i].Allowance = messageRate;
+				//Drop the message
+				if(connectedUsers[i].Allowance < 1.0)
+				    socket.emit('dropped message');
+				else {
+				    connectedUsers[i].Allowance -= 1.0;
+				    io.emit('chat message', msg);
+				}
+			}
+		}
+        //socket.broadcast.emit('chat message', msg);
     });
 	
 	socket.on('have display name', function(name) {
-	    connectedUsers[connectedUsers.length] = {Name: name, ID: socket.id};
+	    connectedUsers[connectedUsers.length] = {Name: name, ID: socket.id, Allowance: messageRate, Check: Date.now()};
 		socket.broadcast.emit('connected user', name);
 	});
 	
 	socket.on('need guest name', function(name) {
 	    var gName = "Guest-" + guestIndex++;
 	    socket.emit('guest name', gName);
-		connectedUsers[connectedUsers.length] = {Name: gName, ID: socket.id};
+		connectedUsers[connectedUsers.length] = {Name: gName, ID: socket.id, Allowance: messageRate, Check: Date.now()};
 		socket.broadcast.emit('connected user', gName); 
 	});
 	
