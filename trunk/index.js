9c86bcb6c19d4546c3ad8c83417006baacb2d437
Cody Averett <codyaverett@gmail.com>
2015-07-09 20:52:29 -0700

    Removing /public directory Static references

--- trunk/index.js
+++ trunk/index.js
@@ -17,23 +17,23 @@ var lastMessages = [];
 var lastMessagesPtrIndex = 0;
 var MESSAGE_QUEUE_SIZE = 1000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){  
+io.on('connection', function(socket){
     socket.on('disconnect', function(data) {
 	    for(var i=0; i<connectedUsers.length; i++) {
 			for(var j=0; j<connectedUsers[i].Sockets.length; j++) {
 				if(socket.id == connectedUsers[i].Sockets[j]) {
 				    connectedUsers[i].Sockets.splice(j, 1);
 					connectedUsers[i].ConnectTimes--;
-					
+
 					if(connectedUsers[i].ConnectTimes == 0) {
-						socket.broadcast.emit('disconnected user', connectedUsers[i]);			
+						socket.broadcast.emit('disconnected user', connectedUsers[i]);
 						connectedUsers.splice(i, 1);
 						socket.broadcast.emit('all connections', connectedUsers);
 					}
@@ -42,11 +42,11 @@ io.on('connection', function(socket){
 			}
 		}
 	});
-   
+
     socket.on('chat message', function(msg, guid){
 	    //Flood Control
 	    for(var i=0; i<connectedUsers.length; i++) {
-		    if(guid == connectedUsers[i].ID) {		
+		    if(guid == connectedUsers[i].ID) {
 				var currentTime = Date.now();
 				var timeElapsed = (currentTime - connectedUsers[i].Check) / 1000.0;
 				connectedUsers[i].Check = currentTime;
@@ -66,20 +66,20 @@ io.on('connection', function(socket){
 					//var h = date.getHours();
 					//var m = date.getMinutes();
 					//var s = date.getSeconds();
-					//var timestamp = '<span class="timestamp noSelect">&nbsp;&nbsp;&nbsp;&nbsp;<span class="month">'+month+'/</span><span class="day">'+day+'&nbsp;</span><span class="time">'+h+':'+m+'.'+s+'</span></span>';				
+					//var timestamp = '<span class="timestamp noSelect">&nbsp;&nbsp;&nbsp;&nbsp;<span class="month">'+month+'/</span><span class="day">'+day+'&nbsp;</span><span class="time">'+h+':'+m+'.'+s+'</span></span>';
 					var timestamp =  '<span class="timestamp noSelect" data-time="'+new Date()+'"></span>';
-					msg = msg.slice(0, msg.length-5) + timestamp + msg.slice(msg.length-5);				
-				
+					msg = msg.slice(0, msg.length-5) + timestamp + msg.slice(msg.length-5);
+
 					lastMessages[lastMessagesPtrIndex] = msg;
 					lastMessagesPtrIndex = (lastMessagesPtrIndex+1)%MESSAGE_QUEUE_SIZE;
-				    connectedUsers[i].Allowance -= 1.0;					
+				    connectedUsers[i].Allowance -= 1.0;
 				    io.emit('chat message', msg);
 				}
 			}
 		}
         //socket.broadcast.emit('chat message', msg);
     });
-	
+
 	socket.on('have display name', function(name, guid) {
 	    for(var i=0; i<connectedUsers.length; i++) {
 		    if(guid == connectedUsers[i].ID) {
@@ -91,11 +91,11 @@ io.on('connection', function(socket){
 	    connectedUsers[connectedUsers.length] = {Name: name, ID: guid, ConnectTimes: 1, Allowance: messageRate, Check: Date.now(), Sockets: [socket.id]};
 		socket.broadcast.emit('connected user', name);
 	});
-	
+
 	socket.on('need guest name', function(guid) {
 /*	    for(var i=0; i<connectedUsers.length; i++) {
 		    if(guid == connectedUsers[i].ID) {
-				connectedUsers[i].ConnectTimes++;				
+				connectedUsers[i].ConnectTimes++;
 				connectedUsers[i].Sockets.push(socket.id);
 				return;
 			}
@@ -103,46 +103,46 @@ io.on('connection', function(socket){
 	    var gName = "Guest-" + guestIndex++;
 	    socket.emit('guest name', gName);
 		connectedUsers[connectedUsers.length] = {Name: gName, ID: guid, ConnectTimes: 1, Allowance: messageRate, Check: Date.now(), Sockets: [socket.id]};
-		socket.broadcast.emit('connected user', gName); 
+		socket.broadcast.emit('connected user', gName);
 	});
-	
+
 	socket.on('need all connections', function(data) {
 	    socket.emit('all connections', connectedUsers);
 	});
-	
+
 	socket.on('new display name', function(name, guid) {
 	    for(var i=0; i<connectedUsers.length; i++) {
 		    if(guid == connectedUsers[i].ID) {
 			    connectedUsers[i].Name = name;
 				io.emit('all connections', connectedUsers);
-				
+
 				for(var j=0; j<connectedUsers[i].Sockets.length; j++) {
 				    if(connectedUsers[i].Sockets[j] != socket.id)
 						io.to(connectedUsers[i].Sockets[j]).emit('you changed names', name);
 				}
 			}
-		}	    
+		}
 	});
-	
+
 	socket.on('is typing', function(choice, guid) {
 	    var user;
 	    for(var i=0; i<connectedUsers.length; i++) {
 		    if(guid == connectedUsers[i].ID) {
 			    user = connectedUsers[i];
-			}	
+			}
 		}
-		
-	    if(choice == "yes") 
+
+	    if(choice == "yes")
 		    socket.broadcast.emit('typing', user);
 		else
 		    socket.broadcast.emit('not typing', user);
 	});
-	
+
 	socket.on('need last messages', function(data) {
 	    socket.emit('last messages', lastMessages, lastMessagesPtrIndex, MESSAGE_QUEUE_SIZE);
 	});
 });
 
-http.listen(3000, function(){
-    console.log('listening on *:3000');
+http.listen(80, function(){
+    console.log('listening on *:80');
 });
